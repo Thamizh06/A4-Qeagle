9886c8d5bd9d475c9f14001e6c18cdfc88db8df1
Thamizh06 <133363660+Thamizh06@users.noreply.github.com>
2025-11-03 23:50:50 +0530

    change in directory

--- Backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Dict
-from dataclasses import dataclass
-
-@dataclass
-class RetrievedDoc:
-    source_id: str          # course_id or jd_id
-    text: str
-    meta: Dict
-    score: float
